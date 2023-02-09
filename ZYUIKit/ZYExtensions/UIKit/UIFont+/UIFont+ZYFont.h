473c17eaf1a53d5922c237e2666e1f7b445436eb
zhangyushaunavayne <shaunavayne@vip.qq.com>
2023-02-09 16:46:41 +0800

    第一次提交

--- ZYUIKit/ZYExtensions/UIKit/UIFont+/UIFont+ZYFont.h
+++ ZYUIKit/ZYExtensions/UIKit/UIFont+/UIFont+ZYFont.h
@@ -6,22 +6,22 @@
 //
 
 /// 设置系统的字体大小（YES：粗体 NO：常规）
-#define zy_Font(__size__,__bold__) ((__bold__)?([UIFont boldSystemFontOfSize:__size__]):([UIFont systemFontOfSize:__size__]))
+#define ZY_Font(__size__,__bold__) ((__bold__)?([UIFont boldSystemFontOfSize:__size__]):([UIFont systemFontOfSize:__size__]))
 
 /// 中等
-#define zy_MediumFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?zy_Font(__size__ , YES):[UIFont zy_pingFangSCMediumWithFontSize:__size__])
+#define ZY_MediumFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?ZY_Font(__size__ , YES):[UIFont ZY_pingFangSCMediumWithFontSize:__size__])
 
 /// 粗体
-#define zy_BoldFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?zy_Font(__size__ , NO):[UIFont zy_pingFangSCBoldWithFontSize:__size__])
+#define ZY_BoldFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?ZY_Font(__size__ , NO):[UIFont ZY_pingFangSCBoldWithFontSize:__size__])
 
 /// 常规
-#define zy_RegularFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?zy_Font(__size__ , NO):[UIFont zy_pingFangSCRegularWithFontSize:__size__])
+#define ZY_RegularFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?ZY_Font(__size__ , NO):[UIFont ZY_pingFangSCRegularWithFontSize:__size__])
 
 /// Heavy
-#define zy_HeavyFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?zy_Font(__size__ , NO):[UIFont zy_pingFangSCHeavyWithFontSize:__size__])
+#define ZY_HeavyFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?ZY_Font(__size__ , NO):[UIFont ZY_pingFangSCHeavyWithFontSize:__size__])
 
 /// Semibold
-#define zy_SemiboldFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?zy_Font(__size__ , NO):[UIFont zy_pingFangSCSemiboldWithFontSize:__size__])
+#define ZY_SemiboldFont(__size__) (([[[UIDevice currentDevice] systemVersion] floatValue]<9.0)?ZY_Font(__size__ , NO):[UIFont ZY_pingFangSCSemiboldWithFontSize:__size__])
 
 #import <UIKit/UIKit.h>
 
@@ -29,15 +29,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIFont (ZYFont)
 
-+ (UIFont *)zy_pingFangSCMediumWithFontSize:(CGFloat)size;
++ (UIFont *)ZY_pingFangSCMediumWithFontSize:(CGFloat)size;
 
-+ (UIFont *)zy_pingFangSCBoldWithFontSize:(CGFloat)size;
++ (UIFont *)ZY_pingFangSCBoldWithFontSize:(CGFloat)size;
 
-+ (UIFont *)zy_pingFangSCRegularWithFontSize:(CGFloat)size;
++ (UIFont *)ZY_pingFangSCRegularWithFontSize:(CGFloat)size;
 
-+ (UIFont *)zy_pingFangSCHeavyWithFontSize:(CGFloat)size;
++ (UIFont *)ZY_pingFangSCHeavyWithFontSize:(CGFloat)size;
 
-+ (UIFont *)zy_pingFangSCSemiboldWithFontSize:(CGFloat)size;
++ (UIFont *)ZY_pingFangSCSemiboldWithFontSize:(CGFloat)size;
 
 @end
 
